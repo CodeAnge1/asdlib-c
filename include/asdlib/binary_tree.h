7ca932015de9ba0c1792d6284910b3ccf4127044
Maksim Mikhailov <87376297+CodeAnge1@users.noreply.github.com>
2025-12-15 01:37:00 +0300

    feat: implement core generic binary tree

---
+++ include/asdlib/binary_tree.h
@@ -0,0 +1,57 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef void* BaseType;
+
+typedef int (*compareFunc)(const BaseType v1, const BaseType v2);
+typedef void (*freeDataFunc)(BaseType data);
+typedef void (*visitFunc)(BaseType data);
+
+typedef struct Node {
+	BaseType data;
+	struct Node* left;
+	struct Node* right;
+} Node;
+
+typedef struct {
+	Node* root;
+	size_t size;
+	compareFunc compare;
+	freeDataFunc freeData;
+} BinaryTree;
+
+typedef enum {
+	BIN_TREE_OK,
+	BIN_TREE_EMPTY,
+	BIN_TREE_NODE_ALLOC_ERR,
+	BIN_TREE_NULL_POINTER,
+	BIN_TREE_INVALID_ARGUMENT,
+	BIN_TREE_NODE_NOT_FOUND,
+	BIN_TREE_LEFT_NODE_EXISTS,
+	BIN_TREE_RIGHT_NODE_EXISTS
+} BinaryTreeError;
+
+BinaryTreeError binaryTreeInit(BinaryTree* tree, 
+							   compareFunc cmp, 
+							   freeDataFunc freeData);
+BinaryTreeError binaryTreeDestroy(BinaryTree* tree);
+
+BinaryTreeError binaryTreeAddLeftChild(BinaryTree* tree, Node* parent, BaseType data);
+BinaryTreeError binaryTreeAddRightChild(BinaryTree* tree, Node* parent, BaseType data);
+
+BinaryTreeError binaryTreeRemoveLeftSubtree(BinaryTree* tree, Node* parent);
+BinaryTreeError binaryTreeRemoveRightSubtree(BinaryTree* tree, Node* parent);
+
+BinaryTreeError binaryTreeInorderTraversal(BinaryTree* tree, visitFunc visit);
+BinaryTreeError binaryTreePreorderTraversal(BinaryTree* tree, visitFunc visit);
+BinaryTreeError binaryTreePostorderTraversal(BinaryTree* tree, visitFunc visit);
+
+size_t binaryTreeSize(BinaryTree* tree);
+
+bool binaryTreeIsLeaf(Node* node);
+bool binaryTreeIsEmpty(BinaryTree* tree);
+
+#endif BINARY_TREE_H
---
+++ src/core/trees/binary_tree.c
@@ -0,0 +1,154 @@
+#include <stdlib.h>
+
+#include "asdlib/binary_tree.h"
+
+static Node* _createNode(BaseType data) {
+	Node* node = (Node*)malloc(sizeof(Node));
+	if (node) {
+		node->left = NULL;
+		node->right = NULL;
+		node->data = data;
+	}
+	return node;
+}
+
+static void _destroySubtree(size_t* size, Node* node, freeDataFunc freeData) {
+    if (node == NULL) return;
+    _destroySubtree(size, node->left, freeData);
+    _destroySubtree(size, node->right, freeData);
+    if (freeData) freeData(node->data);
+	--(*size);
+    free(node);
+}
+
+static void _inorderRecursive(Node* node, visitFunc visit) {
+	if (node->left) _inorderRecursive(node->left, visit);
+	visit(node->data);
+	if (node->right) _inorderRecursive(node->right, visit);
+}
+
+static void _preorderRecursive(Node* node, visitFunc visit) {
+	visit(node->data);
+	if (node->left) _preorderRecursive(node->left, visit);
+	if (node->right) _preorderRecursive(node->right, visit);
+}
+
+static void _postorderRecursive(Node* node, visitFunc visit) {
+	if (node->left) _postorderRecursive(node->left, visit);
+	if (node->right) _postorderRecursive(node->right, visit);
+	visit(node->data);
+}
+
+BinaryTreeError binaryTreeInit(BinaryTree* tree, 
+							   compareFunc cmp, 
+							   freeDataFunc freeData) {
+	BinaryTreeError err = BIN_TREE_OK;
+	if (!tree) {
+		err = BIN_TREE_NULL_POINTER;
+	} else {
+		tree->root = NULL;
+		tree->size = 0;
+		tree->compare = cmp;
+		tree->freeData = freeData;
+	}
+	return err;
+}
+
+BinaryTreeError binaryTreeDestroy(BinaryTree* tree) {
+	BinaryTreeError err = tree ? BIN_TREE_OK : BIN_TREE_NULL_POINTER;
+	if (err == BIN_TREE_OK) {
+        _destroySubtree(&tree->size, tree->root, tree->freeData);
+        tree->root = NULL;
+    }
+    return err;
+}
+
+BinaryTreeError binaryTreeAddLeftChild(BinaryTree* tree, Node* parent, BaseType data) {
+	BinaryTreeError err = parent ? BIN_TREE_OK : BIN_TREE_NULL_POINTER;
+	if (err == BIN_TREE_OK) {
+		Node* node = _createNode(data);
+		if (node) {
+			if (parent->left) {
+				err = BIN_TREE_LEFT_NODE_EXISTS;
+			} else {
+				++tree->size;
+				parent->left = node;
+			}
+		} else {
+			err = BIN_TREE_NODE_ALLOC_ERR;
+		}
+	}
+	return err;
+}
+
+BinaryTreeError binaryTreeAddRightChild(BinaryTree* tree, Node* parent, BaseType data) {
+	BinaryTreeError err = parent ? BIN_TREE_OK : BIN_TREE_NULL_POINTER;
+	if (err == BIN_TREE_OK) {
+		Node* node = _createNode(data);
+		if (node) {
+			if (parent->right) {
+				err = BIN_TREE_RIGHT_NODE_EXISTS;
+			} else {
+				++tree->size;
+				parent->right = node;
+			}
+		} else {
+			err = BIN_TREE_NODE_ALLOC_ERR;
+		}
+	}
+	return err;
+}
+
+BinaryTreeError binaryTreeRemoveLeftSubtree(BinaryTree* tree, Node* parent) {
+	BinaryTreeError err = tree && parent ? BIN_TREE_OK : BIN_TREE_NULL_POINTER;
+	if (err == BIN_TREE_OK) {
+        _destroySubtree(&tree->size, parent->left, tree->freeData);
+        parent->left = NULL;
+    }
+    return err;
+}
+
+BinaryTreeError binaryTreeRemoveRightSubtree(BinaryTree* tree, Node* parent) {
+	BinaryTreeError err = tree && parent ? BIN_TREE_OK : BIN_TREE_NULL_POINTER;
+	if (err == BIN_TREE_OK) {
+        _destroySubtree(&tree->size, parent->right, tree->freeData);
+        parent->right = NULL;
+    }
+    return err;
+}
+
+BinaryTreeError binaryTreeInorderTraversal(BinaryTree* tree, visitFunc visit) {
+	BinaryTreeError err = tree && visit ? BIN_TREE_OK : BIN_TREE_NULL_POINTER;
+	if (tree && tree->root) {
+		_inorderRecursive(tree->root, visit);
+	}
+	return err;
+}
+
+BinaryTreeError binaryTreePreorderTraversal(BinaryTree* tree, visitFunc visit) {
+	BinaryTreeError err = tree && visit ? BIN_TREE_OK : BIN_TREE_NULL_POINTER;
+	if (tree && tree->root) {
+		_preorderRecursive(tree->root, visit);
+	}
+	return err;
+}
+
+BinaryTreeError binaryTreePostorderTraversal(BinaryTree* tree, visitFunc visit) {
+	BinaryTreeError err = tree && visit ? BIN_TREE_OK : BIN_TREE_NULL_POINTER;
+	if (tree && tree->root) {
+		_postorderRecursive(tree->root, visit);
+	}
+	return err;
+}
+
+size_t binaryTreeSize(BinaryTree* tree) {
+	return tree ? tree->size : 0;
+}
+
+bool binaryTreeIsLeaf(Node* node) {
+	return node ? (!node->left && !node->right) : false;
+}
+
+bool binaryTreeIsEmpty(BinaryTree* tree) {
+	return tree ? tree->size == 0 : true;
+}
